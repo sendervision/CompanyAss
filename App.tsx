f4346a9692b510b9b73b41317aab635f02b79f65
sendervision <sendervision@gmail.com>
2024-08-25 05:25:30 +0200

    First commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import "react-native-gesture-handler"
+import React, { useCallback } from 'react';
+import { useFonts} from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { View, Text, StyleSheet } from 'react-native'
+
+SplashScreen.preventAutoHideAsync()
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: require('@/fonts/Inter.ttf'),
+    InterBold: require('@/fonts/InterBold.ttf'),
+    Medium: require("@/fonts/medium.ttf"),
+    Italic: require("@/fonts/italic.ttf")
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return(
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
